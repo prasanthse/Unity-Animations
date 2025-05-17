bcd662a9826330ef251a42afd75fea401ca1619d
Prasanth <prasanth@techserw.com>
2025-05-17 15:34:08 +0530

    MODIFIED: AlienController script name to Alien1DAnimation; ADDED: Acceleration and Deceleration properties for Alien1DAnimation

--- Assets/Scripts/Alien1DAnimation.cs
+++ Assets/Scripts/Alien1DAnimation.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
-public class AlienController : MonoBehaviour
+public class Alien1DAnimation : MonoBehaviour
 {
     private Animator animator;
 
-    private int SPEED = Animator.StringToHash("Speed");
+    private int SPEED_HASH = Animator.StringToHash("Speed");
     private float currentSpeed;
+
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
 
     private void Awake()
     {
@@ -23,28 +26,28 @@ public class AlienController : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                currentSpeed += Time.deltaTime;
+                currentSpeed += Time.deltaTime * acceleration;
                 currentSpeed = Mathf.Clamp(currentSpeed, 0, 1f);
             }
             else if(currentSpeed > 0.5f)
             {
-                currentSpeed -= Time.deltaTime;
+                currentSpeed -= Time.deltaTime * deceleration;
                 currentSpeed = Mathf.Max(currentSpeed, 0.5f);
             }
             else
             {
-                currentSpeed += Time.deltaTime;
+                currentSpeed += Time.deltaTime * acceleration;
                 currentSpeed = Mathf.Clamp(currentSpeed, 0, 0.5f);
             }
 
-            animator.SetFloat(SPEED, currentSpeed);
+            animator.SetFloat(SPEED_HASH, currentSpeed);
         }
         else if(currentSpeed > 0)
         {
-            currentSpeed -= Time.deltaTime;
+            currentSpeed -= Time.deltaTime * deceleration;
             currentSpeed = Mathf.Max(currentSpeed, 0);
 
-            animator.SetFloat(SPEED, currentSpeed);
+            animator.SetFloat(SPEED_HASH, currentSpeed);
         }
     }
 }
