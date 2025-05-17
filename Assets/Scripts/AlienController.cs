449cea5ae19001c392f501de42f7c79835521dbe
Prasanth <prasanth@techserw.com>
2025-05-17 14:17:00 +0530

    ADDED: Alien blentree movement animations via script

--- Assets/Scripts/AlienController.cs
+++ Assets/Scripts/AlienController.cs
@@ -2,5 +2,49 @@ using UnityEngine;
 
 public class AlienController : MonoBehaviour
 {
-    
+    private Animator animator;
+
+    private int SPEED = Animator.StringToHash("Speed");
+    private float currentSpeed;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        currentSpeed = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed += Time.deltaTime;
+                currentSpeed = Mathf.Clamp(currentSpeed, 0, 1f);
+            }
+            else if(currentSpeed > 0.5f)
+            {
+                currentSpeed -= Time.deltaTime;
+                currentSpeed = Mathf.Max(currentSpeed, 0.5f);
+            }
+            else
+            {
+                currentSpeed += Time.deltaTime;
+                currentSpeed = Mathf.Clamp(currentSpeed, 0, 0.5f);
+            }
+
+            animator.SetFloat(SPEED, currentSpeed);
+        }
+        else if(currentSpeed > 0)
+        {
+            currentSpeed -= Time.deltaTime;
+            currentSpeed = Mathf.Max(currentSpeed, 0);
+
+            animator.SetFloat(SPEED, currentSpeed);
+        }
+    }
 }
